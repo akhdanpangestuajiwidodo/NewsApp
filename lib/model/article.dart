ca47b411d022ea562f5f64a0334518ffcfa52639
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2022-02-26 21:19:57 +0700

    create model/class Article

---
+++ lib/model/article.dart
@@ -0,0 +1,18 @@
+class Article {
+  late String author;
+  late String title;
+  late String description;
+  late String url;
+  late String urlToImage;
+  late String publishedAt;
+  late String content;
+
+  Article(
+      {required this.author,
+      required this.title,
+      required this.description,
+      required this.url,
+      required this.urlToImage,
+      required this.publishedAt,
+      required this.content});
+}

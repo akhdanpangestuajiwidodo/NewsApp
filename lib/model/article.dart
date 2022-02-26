8f1489e31b38e3a7c76a557edbd326e15bc75d66
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2022-02-26 21:25:00 +0700

    create named constructor for convert json to article

--- lib/model/article.dart
+++ lib/model/article.dart
@@ -7,12 +7,21 @@ class Article {
   late String publishedAt;
   late String content;
 
-  Article(
-      {required this.author,
-      required this.title,
-      required this.description,
-      required this.url,
-      required this.urlToImage,
-      required this.publishedAt,
-      required this.content});
+  Article({required this.author,
+    required this.title,
+    required this.description,
+    required this.url,
+    required this.urlToImage,
+    required this.publishedAt,
+    required this.content});
+
+ Article.fromJson(Map<String, dynamic> article){
+   author = article['author'];
+   title = article['title'];
+   description = article['description'];
+   url = article['url'];
+   urlToImage = article['urlToImage'];
+   publishedAt = article['publishedAt'];
+   content = article['content'];
+ }
 }

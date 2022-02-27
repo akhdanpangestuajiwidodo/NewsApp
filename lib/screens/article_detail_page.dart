6d31ee8d72e8c5f0461ef585aa1e30836ce91c11
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2022-02-27 09:59:52 +0700

    Update component detail page

--- lib/screens/article_detail_page.dart
+++ lib/screens/article_detail_page.dart
@@ -6,11 +6,53 @@ class ArticleDetailPage extends StatelessWidget {
 
   final Article article;
 
-
   ArticleDetailPage({required this.article});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(article.title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.network(article.urlToImage),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(article.description),
+                  const Divider(color: Colors.grey),
+                  Text(
+                    article.title,
+                    style: const TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24,
+                    ),
+                  ),
+                  const Divider(color: Colors.grey),
+                  Text('Date: ${article.publishedAt}'),
+                  const SizedBox(height: 10),
+                  Text('Author: ${article.author}'),
+                  const Divider(color: Colors.grey),
+                  Text(
+                    article.content,
+                    style: const TextStyle(fontSize: 16),
+                  ),
+                  const SizedBox(height: 10),
+                  ElevatedButton(
+                    child: const Text('Read more'),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }

0d5efed0dbbc96dad12e3f9400f51b77a93c8baf
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2022-02-27 09:57:39 +0700

    initial detail page

---
+++ lib/screens/article_detail_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/model/article.dart';
+
+class ArticleDetailPage extends StatelessWidget {
+  static const routeName = '/article_detail';
+
+  final Article article;
+
+
+  ArticleDetailPage({required this.article});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

65dbe382cc8df5c87a6eaf401e5fbbef9e25f101
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2022-03-06 09:52:29 +0700

    full code custom scaffold

--- lib/widgets/custom_scaffold.dart
+++ lib/widgets/custom_scaffold.dart
@@ -4,11 +4,44 @@ import 'package:flutter/material.dart';
 class CustomScaffold extends StatelessWidget {
   final Widget body;
 
-
   CustomScaffold({required this.body});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            body,
+            Card(
+              margin: const EdgeInsets.all(0),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    icon: Icon(Icons.arrow_back),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 16.0),
+                    child: Text(
+                      'N',
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                  )
+                ],
+              ),
+              shape: BeveledRectangleBorder(
+                borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(16.0)
+                )
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
